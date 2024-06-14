053ce8ca5a67451b09ff030b3c16154830188b43
Ben Saacks <108644584+SirParadox42@users.noreply.github.com>
2024-06-14 16:35:23 -0700

    Update NewPost.js

--- frontend-mobile/screens/NewPost.js
+++ frontend-mobile/screens/NewPost.js
@@ -11,9 +11,9 @@ import Loading from '../components/Loading';
 export default function NewPost(props) {
     const ctx = useContext(context);
     const [isLoading, sendRequest] = useHttp();
-    const [titleInput, titleValid, handleTitleChange, handleTitleBlur, handleTitleSubmit, titleInvalid, setTitleInput] = useInput(input => input.length > 0 && input.length <= 20);
+    const [titleInput, titleValid, handleTitleChange, handleTitleBlur, handleTitleSubmit, titleInvalid, setTitleInput, setTitleTouched] = useInput(input => input.length > 0 && input.length <= 20);
     const [selectedImage, setSelectedImage] = useState(null);
-    const [descriptionInput, descriptionValid, handleDescriptionChange, handleDescriptionBlur, handleDescriptionSubmit, descriptionInvalid, setDescriptionInput] = useInput(input => input.length > 0 && input.length <= 300);
+    const [descriptionInput, descriptionValid, handleDescriptionChange, handleDescriptionBlur, handleDescriptionSubmit, descriptionInvalid, setDescriptionInput, setDescriptionTouched] = useInput(input => input.length > 0 && input.length <= 300);
     
     const handleSelectImage = image => setSelectedImage(image);
     const createPost = async() => {
@@ -23,7 +23,7 @@ export default function NewPost(props) {
         if (titleValid && selectedImage && descriptionValid) {
             const formData = new FormData();
             formData.append('title', titleInput);
-            formData.append('image', {uri: selectedImage.uri, type: 'image/jpeg', name: 'profile.jpeg', size: selectedImage.fileSize});
+            formData.append('image', {uri: selectedImage.uri, type: 'image/jpeg', size: selectedImage.fileSize});
             formData.append('description', descriptionInput);
             formData.append('createdAt', new Date().toLocaleDateString());
             
@@ -39,8 +39,10 @@ export default function NewPost(props) {
     useEffect(() => {
         const cleanup = props.navigation.addListener('focus', () => {
             setTitleInput('');
+            setTitleTouched(false);
             setSelectedImage(null);
             setDescriptionInput('');
+            setDescriptionTouched(false);
         });
 
         return cleanup
@@ -49,9 +51,9 @@ export default function NewPost(props) {
     return (
         <ScrollView style={styles.form} alwaysBounceVertical={false}>
             <Loading isLoading={isLoading} style={styles.loading}/>
-            <Input message={titleInvalid ? 'Input length must be between 1 and 20 characters.' : ''} input={{placeholder: 'Title', autoCorrect: false, autoCapitalize: false, value: titleInput, onChangeText: handleTitleChange, onBlur: handleTitleBlur}}/>
+            <Input message={titleInvalid ? 'Input must be between 1 and 20 characters.' : ''} input={{placeholder: 'Title', autoCorrect: false, autoCapitalize: false, value: titleInput, onChangeText: handleTitleChange, onBlur: handleTitleBlur}}/>
             <ImagePicker onSelectImage={handleSelectImage} selectedImage={selectedImage}/>
-            <Input message={descriptionInvalid ? 'Input length must be between 1 and 300 characters.' : ''} input={{placeholder: 'Description', autoCorrect: false, autoCapitalize: false, multiline: true, value: descriptionInput, onChangeText: handleDescriptionChange, onBlur: handleDescriptionBlur}}/>
+            <Input message={descriptionInvalid ? 'Input must be between 1 and 300 characters.' : ''} input={{placeholder: 'Description', autoCorrect: false, autoCapitalize: false, multiline: true, value: descriptionInput, onChangeText: handleDescriptionChange, onBlur: handleDescriptionBlur}}/>
             <Button onPress={createPost}>Create Post</Button>
         </ScrollView>
     );
